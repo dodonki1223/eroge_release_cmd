296a83b3d268f5737d0cea03f098e2428728992e
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2019-03-06 22:00:27 +0900

    :recycle: release_list_scraping_specのリファクタリング

--- spec/getchuya_scraping/release_list_scraping_spec.rb
+++ spec/getchuya_scraping/release_list_scraping_spec.rb
@@ -8,20 +8,21 @@ describe ReleaseListScraping do
 
   describe '#scraping' do
     let(:release_list) do
-      VCR.use_cassette 'release_list_scraping_release_list' do
+      VCR.use_cassette 'release_list' do
         release_list_scraping.scraping
       end
     end
+    let(:first_elemet) { release_list[0] }
 
     # release_listが配列で１つ目の要素がHashで６つのKeyを持つかテスト
-    it { expect(release_list[0]).to include(:release_date, :title, :introduction_page, :id, :brand_name, :price) }
+    it { expect(first_elemet).to include(:release_date, :title, :introduction_page, :id, :brand_name, :price) }
 
     # スクレイピングした結果、値が取得できていることをテスト
-    it { expect(release_list[0][:release_date]).not_to eq '' }
-    it { expect(release_list[0][:title]).not_to eq '' }
-    it { expect(release_list[0][:introduction_page]).not_to eq '' }
-    it { expect(release_list[0][:id]).not_to eq '' }
-    it { expect(release_list[0][:brand_name]).not_to eq '' }
-    it { expect(release_list[0][:price]).not_to eq '' }
+    it { expect(first_elemet[:release_date]).not_to eq '' }
+    it { expect(first_elemet[:title]).not_to eq '' }
+    it { expect(first_elemet[:introduction_page]).not_to eq '' }
+    it { expect(first_elemet[:id]).not_to eq '' }
+    it { expect(first_elemet[:brand_name]).not_to eq '' }
+    it { expect(first_elemet[:price]).not_to eq '' }
   end
 end
